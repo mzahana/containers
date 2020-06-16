848b7862dbdf1f69b452968a90df770983e9ed4f
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2020-06-16 20:00:07 +0400

    Add script for setting up Docker

---
+++ scripts/setup_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Installing Docker ..."
+# Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+
+echo "Allowing docker to run as non-root user ..."
+sudo usermod -aG docker ${USER}
+echo "aaal" | su -l ${USER}
+
+
+echo "Setting up Nvidia docker ..."
+# Source: https://github.com/NVIDIA/nvidia-docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+echo "Installing nvidia-container-runtime ..."
+sudo apt install nvidia-container-runtime -y
