cd3b0f2b0fcb636ea9273fefe17ebc88dc239b91
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2020-07-11 02:16:15 +0400

    Update setup_docker.sh

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "Installing Docker ..."
+read -p "Enter user password please: " -s pass
 # Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+echo $pass | sudo -S apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
+echo $pass | sudo -S add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo $pass | sudo -S apt-get update -y
+echo $pass | sudo -S apt-get install docker-ce -y
 
 echo "Allowing docker to run as non-root user ..."
-sudo usermod -aG docker ${USER}
-echo "aaal" | su -l ${USER}
+echo $pass | sudo -S usermod -aG docker ${USER}
+echo $pass | su -l ${USER}
 
 
 echo "Setting up Nvidia docker ..."
@@ -18,8 +19,9 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-sudo systemctl restart docker
+echo $pass | sudo -S apt-get update -y
+echo $pass | sudo -S apt-get install -y nvidia-container-toolkit
+echo $pass | sudo -S systemctl restart docker
 
 echo "Installing nvidia-container-runtime ..."
-sudo apt install nvidia-container-runtime -y
+echo $pass | sudo -S apt-get install nvidia-container-runtime -y
