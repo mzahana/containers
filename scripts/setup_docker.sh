a350c4b1c78595acc69fe32f9f915e0d1a46895e
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2020-12-05 08:19:43 +0400

    Update setup_docker.sh

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# To change the printing colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
 echo "Installing Docker ..."
 read -p "Enter user password please: " -s pass
 # Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
@@ -8,20 +14,37 @@ echo $pass | sudo -S add-apt-repository "deb [arch=amd64] https://download.docke
 echo $pass | sudo -S apt-get update -y
 echo $pass | sudo -S apt-get install docker-ce -y
 
-echo "Allowing docker to run as non-root user ..."
+echo -e "${RED}Adding new group 'docker' ...${NC}"
+echo $pass | sudo -S groupadd docker
+
+echo -e "${RED}Allowing docker to run as non-root user ...${NC}"
+echo -e "${RED}Adding $USER in docker group...${NC}"
 echo $pass | sudo -S usermod -aG docker ${USER}
-echo $pass | su -l ${USER}
 
+#### NOTE: The following 2 commands will start in a new shell 
+#        and the rest of the script will not execute untill you exit that shell
+#        So, we advise to re-login again manually
+# newgrp docker
+# echo $pass | su -l ${USER}
 
-echo "Setting up Nvidia docker ..."
+echo $pass | sudo -S systemctl enable docker
+
 # Source: https://github.com/NVIDIA/nvidia-docker
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 echo $pass | sudo -S apt-get update -y
-echo $pass | sudo -S apt-get install -y nvidia-container-toolkit
+echo -e "${RED}Installing nvidia-docker2 ...${NC}"
+echo $pass | sudo -S apt-get install -y nvidia-docker2
+# echo $pass | sudo -S apt-get install -y nvidia-container-toolkit
+
+# echo "Installing nvidia-container-runtime ..."
+# echo $pass | sudo -S apt-get install nvidia-container-runtime -y
+
+echo -e "${RED}Restarting Docker daeomn...${NC}"
 echo $pass | sudo -S systemctl restart docker
 
-echo "Installing nvidia-container-runtime ..."
-echo $pass | sudo -S apt-get install nvidia-container-runtime -y
+
+
+echo -e "${RED} ****************** Make sure to logout and login again for docker to take effect ****************** ${NC}"
