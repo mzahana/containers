19a7e15296c65c1dac24c8f7c84c63f9140dec67
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2021-09-23 12:01:24 +0300

    Add run_kilbr.sh

---
+++ scripts/run_kalibr.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Runs Kalibr inside a Docker container
+# Reference: https://github.com/ethz-asl/kalibr/wiki/installation#c-using-through-docker
+
+data_dir=$1
+
+if [ ! -d "$data_dir" ]; then
+  echo "data directory does not exist: $data_dir"
+  exit 1
+fi
+
+xhost +local:root;
+docker run -it -e DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $data_dir:/root/data stereolabs/kalibr:kinetic /bin/bash -c "cd /root/data; /bin/bash"
+xhost -local:root;
